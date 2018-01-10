3d3f0fa03597cc074951037f80f17d69129df682
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-10 14:33:03 +0530

    added destination bucket

--- S3TutorialAmazon/S3TutorialAmazon-lambda.js
+++ S3TutorialAmazon/S3TutorialAmazon-lambda.js
@@ -1,5 +1,6 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback){
+const s3 = new AWS.S3();
+exports.handler = function (event, context, callback) {
 
     //Entered in to Lambda
     console.log("Starting the lambda");
@@ -8,8 +9,29 @@ exports.handler = function(event, context, callback){
     var srcBucket = event.Records[0].s3.bucket.name;
 
     //Get source key
-    var srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g,""));
+    var srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ""));
 
     console.log(srcBucket);
     console.log(srcKey);
+
+    s3.putObject({
+        "Body": "data",
+        "Bucket": "hiru.sampleresized",
+        "Key": "samplekey"
+    })
+        .promise()
+        .then(data => {
+            console.log(data);           // successful response
+            /*
+            data = {
+                ETag: "\\"6805f2cfc46c0f04559748bb039d69ae\\"", 
+                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
+            }
+            */
+        })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
+        });
+
+
 }
