8540f6098f64d81ffe909d5a22c7e0bf62ffbf3d
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-11 12:28:48 +0530

    clear commit

--- S3TutorialAmazon/S3TutorialAmazon-lambda.js
+++ S3TutorialAmazon/S3TutorialAmazon-lambda.js
@@ -11,18 +11,18 @@ exports.handler = function (event, context, callback) {
     //Get source key
     var srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ""));
 
-    console.log("Source Bucket name"+srcBucket);
-    console.log("Source Bucket key"+srcKey);
+    console.log("Source Bucket name" + srcBucket);
+    console.log("Source Bucket key" + srcKey);
 
     //Get uploade Image type
     var typeMatch = srcKey.match(/\.([^.]*)$/);
 
     //No image type to determine
-    if(!typeMatch){
+    if (!typeMatch) {
         callback("Could not determine the image type");
         return;
     }
-    console.log("Image type"+typeMatch);
+    console.log("Image type" + typeMatch);
 
     //Image type is not supported
     var imageType = typeMatch[1];
@@ -30,25 +30,6 @@ exports.handler = function (event, context, callback) {
         callback('Unsupported image type: ${imageType}');
         return;
     }
-    
-    s3.putObject({
-        "Body": "data",
-        "Bucket": "hiru.sampleresized",
-        "Key": "samplekey"
-    })
-        .promise()
-        .then(data => {
-            console.log(data);           // successful response
-            /*
-            data = {
-                ETag: "\\"6805f2cfc46c0f04559748bb039d69ae\\"", 
-                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
-            }
-            */
-        })
-        .catch(err => {
-            console.log(err, err.stack); // an error occurred
-        });
 
 
 }
